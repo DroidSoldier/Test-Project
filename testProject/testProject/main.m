45ff50c30c2ec93978d23ac0ace01986bd12eb98
Paul Devey <paul@Pauls-MacBook-Pro.local>
2015-12-07 18:31:58 -0500

    Initial Commit

---
+++ testProject/testProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testProject
+//
+//  Created by Paul on 12/7/15.
+//  Copyright © 2015 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
